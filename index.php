a7d746dcec8a82094eae4667f9685444a2596814
HMate2 <EX8S2M@hallgato.nje.hu>
2024-11-02 17:52:13 +0100

    Weboldal séma kialakítása

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Hangosfilmek</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span class="icon fa-gem"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>Hangosfilmek</h1>
+								<p>A két világháború között hazánkban virágzott a filmipar. A hangosfilm térhódításával egyre több, évente akár több tucat film is készült. <br> Az adatbázis ezen filmek főbb adatait dolgozza fel ezen a reszponzív témájú, RESTFUL webszolgáltatáson keresztül. <br />
+								</p>
+							</div>
+						</div>
+						<nav>
+							<ul>
+								<li><a href="#mnb">MNB</a></li>
+								<li><a href="#pdf">PDF</a></li>
+								<li><a href="#soap">SOAP</a></li>
+								<li><a href="#restful">RESTFUL</a></li>
+								<li><a href="#contact">Bejelentkezés</a></li>
+						
+								<!--<li><a href="#elements">Elements</a></li>-->
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- MNB -->
+							<article id="mnb">
+								<h2 class="major">MNB</h2>
+								
+								<p>//mnb soap adatszolg</p>
+								
+							</article>
+
+						<!-- PDF -->
+							<article id="pdf">
+								<h2 class="major">PDF</h2>
+								
+								<p>pdf tcppdf Db 3</p>
+								
+							</article>
+
+						<!-- Soap -->
+							<article id="soap">
+								<h2 class="major">SOAP</h2>
+								
+								<p>restful soap szerver + kliens</p>
+							</article>
+
+						<!-- Restful -->
+						<article id="restful">
+								<h2 class="major">RESTFUL</h2>
+								
+								<p>restfulGetPostPutDelete; cURL Postman</p>
+							</article>	
+
+						<!-- Contact -->
+							<article id="contact">
+								<h2 class="major">Bejelentkezés</h2>
+								<form method="post" action="#">
+									<div class="fields">
+										<div class="field half">
+											<label for="name">Név</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="passrord">Jelszó</label>
+											<input type="password" name="password" id="passwrod" />
+										</div>
+										
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Rendben" class="primary" /></li>
+										<a href="#regi"><li><input type="button" value="Regisztráció" /></li></a>
+										
+									</ul>
+								</form>
+								
+							</article>
+						<!-- Regi -->
+						<article id="regi">
+								<h2 class="major">Regisztráció</h2>
+								<form method="post" action="#">
+									<div class="fields">
+										<div class="field half">
+											<label for="name">Név</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="passrord">Jelszó</label>
+											<input type="password" name="password" id="passwrod" />
+										</div>
+										
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Rendben" class="primary" /></li>
+										<li><input type="reset" value="Reset" /></li>
+									</ul>
+								</form>
+								
+							</article>
+									
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">GAMF NJE Hujber Máté</p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
